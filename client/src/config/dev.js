34a008750ad457b503a148410f876a3c9efde28b
florian lahitte <lahitte.florian@gmail.com>
2020-02-25 08:23:34 -0500

    update

--- client/src/config/dev.js
+++
@@ -1,7 +0,0 @@
-// dev.js - don't commit this !!!
-module.exports = {
-  siteUrl: "http://localhost:3001",
-  cloudinaryClientName: "dwtc6zep7",
-  cloudinaryClientID: "596555837329564",
-  cloudinaryClientSecret: "oCn37LxVL2KbWdFIU_H89yVcz6o"
-};
